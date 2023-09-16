5eff57eae3ed990cbb6a327ff2bf12ae188aab25
nohahamdy <nohaelsheweikh@gmail.com>
2023-09-16 11:02:59 +0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Noha Hamdy on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
