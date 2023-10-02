7c65c73a1394c2d86935da49bd4649946963e344
nohahamdy <nohaelsheweikh@gmail.com>
2023-10-02 22:22:42 +0300

    alternate app icons

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -8,7 +8,17 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: - PROPERTIES
+    private let alternateAppIcons: [String] = [
+      "AppIcon-MagnifyingGlass",
+      "AppIcon-Map",
+      "AppIcon-Mushroom",
+      "AppIcon-Camera",
+      "AppIcon-Backpack",
+      "AppIcon-Campfire"
+    ]
     var body: some View {
+     
         List{
             //MARK: - SECTION HEADER
             Section{
@@ -50,8 +60,45 @@ struct SettingsView: View {
                 .frame(maxWidth: .infinity)
             }//: HEADER
             .listRowSeparator(.hidden)
+            
             //MARK: - SECTION: ICONS
             
+            Section(header:Text("Alternate Icons")){
+                ScrollView(.horizontal,showsIndicators: false){
+                    
+                    HStack(spacing:12){
+                        ForEach(alternateAppIcons.indices, id:\.self) { item in
+                            Button{
+                                print("Icon \(alternateAppIcons[item]) was pressed")
+                                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]){
+                                    error in
+                                    if error != nil {
+                                        print("failed request to update the app's icon \(String(describing: error?.localizedDescription)) ")
+                                    }else{
+                                        print("Success! You have changed the app's icon to \(alternateAppIcons[item])")
+                                    }
+                                }
+                            }label: {
+                                Image("\(alternateAppIcons[item])-Preview")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 80)
+                                    .cornerRadius(16)
+                            }
+                        .buttonStyle(.borderless)
+                        }
+                    }
+                } // SCROLL VIEW
+                .padding(.top,12)
+                Text("Choose your favourite app icon from the collection above.")
+                    .frame(minWidth: 0,maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    .padding(.bottom,12)
+                
+            }//: Section
+            .listRowSeparator(.hidden)
             
             //MARK: - SECTION: ABOUT
             Section (
@@ -63,8 +110,8 @@ struct SettingsView: View {
                 }.padding(.vertical,8)
             ){
                 // 1. Basic LabeledContent
-//                LabeledContent("Application",value:"Hike")
-//
+                //LabeledContent("Application",value:"Hike")
+
                 
                 // 2. Advanced Labeled content
                 CustomListRowView(
